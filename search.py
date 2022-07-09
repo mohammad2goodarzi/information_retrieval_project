2d3f46c9063d1570cfeffa82ccf7e45b20652e6e
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2022-07-09 05:46:32 +0430

    implement search for two-word queries

--- search.py
+++ search.py
@@ -1,3 +1,6 @@
+from indexing import process_documents
+
+
 def normalize_prefix_suffix(word: str):
     """
     Take a word and remove its prefix and suffix.
@@ -25,7 +28,7 @@ def search(word):
     Take a word and return the list of document IDs containing that word.
     """
     # posting_lists = process_documents()
-    posting_lists = {}
+    # posting_lists = {}
     word = normalize_prefix_suffix(word)
     if word not in posting_lists:
         return 'no result found.'
@@ -35,11 +38,25 @@ def search(word):
 
 def process_query(query):
     """
-    Take a query and return the list of document IDs containing that word.
+    Take a query and return a list of document IDs containing that word.
+    A query can contain one word or two words along with the keyword "and" or "or".
     """
-    return search(query)
+    if 'and' in query:
+        word1, word2 = query.split('and')
+        word1 = word1.strip()
+        word2 = word2.strip()
+        return list(set(search(word1)).intersection(set(search(word2))))
+    if 'or' in query:
+        word1, word2 = query.split('or')
+        word1 = word1.strip()
+        word2 = word2.strip()
+        return list(set(search(word1)).union(set(search(word2))))
+    else:
+        return search(query)
 
 
+posting_lists = process_documents()  # this is temporary
 if __name__ == '__main__':
     print("جهان", process_query("چهان"))
     print("بهداشت", process_query("بهداشت"))
+    print("بهداشت and جهان", process_query("بهداشت and جهان"))
